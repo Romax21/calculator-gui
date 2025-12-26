37b77e79c29fe76b12e3a6397b21c53c64c7cc0e
Ronaldo <rykendocool21@gmail.com>
2025-12-26 16:33:34 +0530

    created input and output box with readonly displays

--- calculator.py
+++ calculator.py
@@ -1,8 +1,18 @@
 import tkinter as tk
 
 root = tk.Tk()
-
-root.geometry("500x700")
+root.geometry("400x600")
 root.title("Calculator GUI")
 
+input_field = tk.StringVar()
+inputEntry = tk.Entry(root,width=40,textvariable=input_field,justify='right',font=('arial',20),state='readonly')
+input_field.set("111+222")
+
+inputEntry.pack(padx=10,pady=(20,10))
+
+output_field = tk.StringVar()
+outputEntry = tk.Entry(root,width=40,textvariable=output_field,justify='right',font=('arial',20),state='readonly')
+output_field.set("333")
+
+outputEntry.pack(padx=10,pady=10)
 root.mainloop()
