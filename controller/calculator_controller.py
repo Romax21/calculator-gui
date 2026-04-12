2de32ea8ec4e3d897b1a0143713615423dbd9947
Ronaldo <rykendocool21@gmail.com>
2026-04-12 18:49:44 +0530

    calculator_controller created

---
+++ controller/calculator_controller.py
@@ -0,0 +1,26 @@
+from model import calculator_engine
+class calculator_controller : 
+    def __init__(self):
+        self.expression = ""
+        self.result = ""
+        self.ce = calculator_engine()
+    
+    def buttonPressed(self,text) : 
+        self.expression = ""
+        self.result = ""
+        if text in '1234567890': 
+            self.expression = self.ce.addNumber(text)
+        elif text == '.' : 
+            self.expression = self.ce.addDecimal()
+        elif text in '+-*/%' : 
+            self.expression = self.ce.addOperator(text)
+        elif text == '1/x' : 
+            self.result = self.ce.reciprocal(text)
+        elif text == 'x^2' : 
+            self.result = self.ce.square(text)
+        elif text == '<=' : 
+            self.expression = self.ce.backspace()
+        elif text == '=' : 
+            self.result = self.ce.equalCalled()
+        
+        return (self.expression,self.result)
