d0c0bde4ce43a437ea576b1df761de84f3907188
Ronaldo <rykendocool21@gmail.com>
2026-04-14 20:27:24 +0530

    view changed to a class

--- view/calculator_view.py
+++ view/calculator_view.py
@@ -1,54 +1,56 @@
 import tkinter as tk
-from controller import calculator_controller
+from controller.calculator_controller import calculator_controller
 
-cc = calculator_controller.calculator_controller()
+class calculator_view : 
+    def __init__(self,controller) : 
+        self.controller = calculator_controller()
+        self.root = tk.Tk()
+        self.root.geometry("400x600")
+        self.root.title("Calculator GUI")
+        
+        self.input_field = tk.StringVar()
+        self.inputEntry = tk.Entry(self.root,width=40,textvariable=self.input_field,justify='right',font=('arial',20),state='readonly')
+        self.input_field.set("")
+        self.inputEntry.pack(padx=10,pady=(20,10),fill='x')
+        
+        self.output_field = tk.StringVar()
+        self.outputEntry = tk.Entry(self.root,width=40,textvariable=self.output_field,justify='right',font=('arial',20),state='readonly')
+        self.output_field.set("")
+        self.outputEntry.pack(padx=10,pady=10,fill='x')
+        
+        self.btn_style = {'font':('arial',20), 'bd':2, 'relief':'raised', 'bg':'white', 'fg':'black'}
+        
+        self.resetBtn = tk.Button(self.root,text = 'Reset',command=lambda t='Reset':self.buttonPressed(t), **self.btn_style)
+        
+        self.resetBtn.pack(padx = 10,pady=5,fill = 'x')
 
-def buttonPressed(text) : 
-    expression,result = cc.buttonPressed(text)
-    input_field.set(expression)
-    output_field.set(result)
-    return
+        self.buttonFrame = tk.Frame(self.root,bg='lightgrey')
+        self.buttons = []
+        self.button_defs = [
+            ('%', 0, 0), ('1/x', 0, 1), ('x^2', 0, 2), ('<=', 0, 3),
+            ('1', 1, 0), ('2', 1, 1), ('3', 1, 2), ('+', 1, 3),
+            ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('-', 2, 3),
+            ('7', 3, 0), ('8', 3, 1), ('9', 3, 2), ('*', 3, 3),
+            ('.', 4, 0), ('0', 4, 1), ('=', 4, 2), ('/', 4, 3)
+        ]
+        
+        for (text,row,col) in self.button_defs : 
+            self.btn = tk.Button(self.buttonFrame,text=text,command=lambda t=text:self.buttonPressed(t),**self.btn_style)
+            self.btn.grid(row=row,column=col,sticky='nsew')
+            self.buttons.append(self.btn)
 
-root = tk.Tk()
-root.geometry("400x600")
-root.title("Calculator GUI")
+        for i in range(4) : 
+            self.buttonFrame.columnconfigure(i,weight=1)
+        for i in range(5) : 
+            self.buttonFrame.rowconfigure(i,weight=1)
 
-input_field = tk.StringVar()
-inputEntry = tk.Entry(root,width=40,textvariable=input_field,justify='right',font=('arial',20),state='readonly')
-input_field.set("")
-inputEntry.pack(padx=10,pady=(20,10),fill='x')
+        self.buttonFrame.pack(padx=10,pady=10,fill='both',expand=True)
 
-output_field = tk.StringVar()
-outputEntry = tk.Entry(root,width=40,textvariable=output_field,justify='right',font=('arial',20),state='readonly')
-output_field.set("")
-outputEntry.pack(padx=10,pady=10,fill='x')
-
-btn_style = {'font':('arial',20), 'bd':2, 'relief':'raised', 'bg':'white', 'fg':'black'}
-
-resetBtn = tk.Button(root,text = 'Reset',command=lambda t='Reset':buttonPressed(t), **btn_style)
-resetBtn.pack(padx = 10,pady=5,fill = 'x')
-
-buttonFrame = tk.Frame(root,bg='lightgrey')
-buttons = []
-button_defs = [
-    ('%', 0, 0), ('1/x', 0, 1), ('x^2', 0, 2), ('<=', 0, 3),
-    ('1', 1, 0), ('2', 1, 1), ('3', 1, 2), ('+', 1, 3),
-    ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('-', 2, 3),
-    ('7', 3, 0), ('8', 3, 1), ('9', 3, 2), ('*', 3, 3),
-    ('.', 4, 0), ('0', 4, 1), ('=', 4, 2), ('/', 4, 3)
-]
-
-for (text,row,col) in button_defs : 
-    btn = tk.Button(buttonFrame,text=text,command=lambda t=text:buttonPressed(t),**btn_style)
-    btn.grid(row=row,column=col,sticky='nsew')
-    buttons.append(btn)
-
-
-for i in range(4) : 
-    buttonFrame.columnconfigure(i,weight=1)
-for i in range(5) : 
-    buttonFrame.rowconfigure(i,weight=1)
-
-buttonFrame.pack(padx=10,pady=10,fill='both',expand=True)
-
-root.mainloop()
+    def buttonPressed(self,text) : 
+        expression,result = self.controller.buttonPressed(text)
+        self.input_field.set(expression)
+        self.output_field.set(result)
+        return
+    
+    def run(self) : 
+        self.root.mainloop()
