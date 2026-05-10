a4d156a02b57a8ffbf5c7ee2ce72e62cb54d975f
Ronaldo <rykendocool21@gmail.com>
2026-05-10 19:00:35 +0530

    two test cases added

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,6 @@
 import pytest
 from main.model.calculator_engine import calculator_engine
+from main.utils.constants import ZERO_DIVISION_MESSAGE
 
 @pytest.fixture
 def engine_object() : 
@@ -9,3 +10,34 @@ def test_expression1(engine_object) :
     assert engine_object.addNumber('0') == "0"
     assert engine_object.addNumber('2') == "2"
     assert engine_object.addNumber('3') == "23"
+    assert engine_object.addOperator('+') == "23+"
+    assert engine_object.addDecimal() == "23+0."
+    assert engine_object.addNumber('9') == "23+0.9"
+    
+    expression,result = engine_object.equalCalled()
+    assert expression == ""
+    assert result == "23.9"
+
+def test_expression2(engine_object) : 
+    engine_object.addOperator('-')
+    engine_object.addNumber('2')
+    engine_object.addNumber('5')
+    engine_object.addOperator('*')
+    engine_object.addOperator('-')
+    engine_object.addNumber('2')
+    engine_object.addNumber('5')
+    
+    assert engine_object.inputResult() == "-25*-25"
+    
+    ex,result = engine_object.equalCalled()
+    assert ex == ""
+    assert result == "625"
+
+def test_expression3(engine_object) : 
+    engine_object.addOperator('-')
+    engine_object.addNumber('2')
+    engine_object.addNumber('5')
+    
+    ex,result = engine_object.reciprocal()
+    assert ex == ""
+    assert result == "-0.04"
