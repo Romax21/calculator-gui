8c1b7b45d536505e1118bce01629ed43ec34c2cd
Ronaldo <rykendocool21@gmail.com>
2026-05-17 10:27:28 +0530

    Engine updated to match new calculator_logic

--- main/model/calculator_engine.py
+++ main/model/calculator_engine.py
@@ -133,7 +133,7 @@ class calculator_engine :
         return self.inputResult()
     
     def equalCalled(self) : 
-        result = calculator_logic.calculate_expression(self.inputResult())
+        result = calculator_logic.calculate_expression(self.opA,self.oper,self.opB)
         if result != constants.ZERO_DIVISION_MESSAGE and result : 
             self.justEvaluated = True
             self.opA = result
