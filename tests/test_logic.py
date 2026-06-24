2985db45f942fa83ea1280488a4648092f2e1b7d
Ronaldo <rykendocool21@gmail.com>
2026-06-24 21:55:16 +0530

    Updated test_logic

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -3,40 +3,40 @@ from main.model.calculator_logic import calculate_expression
 from main.utils.constants import ZERO_DIVISION_MESSAGE
 
 @pytest.mark.parametrize(
-    "expression, expected",
+    "opA,oper,opB,expected",
     [
-        ("2+3","5"),
-        ("7-4","3"),
-        ("6*5","30"),
-        ("8/2","4"),
-        ("4.2%3","1.2")
+        ("2","+","3","5"),
+        ("7","-","4","3"),
+        ("6","*","5","30"),
+        ("8","/","2","4"),
+        ("4.2","%","3","1.2")
     ]
 )
-def test_basicOperations(expression,expected) : 
-    result = calculate_expression(expression)
+def test_basicOperations(opA,oper,opB,expected) : 
+    result = calculate_expression(opA,oper,opB)
     assert result == expected
 
 @pytest.mark.parametrize(
-    "expression, expected",
+    "opA,oper,opB,expected",
     [
-        ("2.5+1.5","4"),
-        ("5.5-2.2","3.3"),
-        ("3.2*2","6.4"),
-        ("7.5/2.5","3"),
-        ("0.1+0.2","0.3")
+        ("2.5","+","1.5","4"),
+        ("5.5","-","2.2","3.3"),
+        ("3.2","*","2","6.4"),
+        ("7.5","/","2.5","3"),
+        ("0.1","+","0.2","0.3")
     ]
 )
-def test_decimalOperations(expression,expected) : 
-    result = calculate_expression(expression)
+def test_decimalOperations(opA,oper,opB,expected) : 
+    result = calculate_expression(opA,oper,opB)
     assert result == expected
 
 @pytest.mark.parametrize(
-    "expression, expected",
+    "opA,oper,opB,expected",
     [
-        ("1/0",ZERO_DIVISION_MESSAGE),
-        ("23%0",ZERO_DIVISION_MESSAGE)
+        ("1","/","0",ZERO_DIVISION_MESSAGE),
+        ("23","%","0",ZERO_DIVISION_MESSAGE)
     ]
 )
-def test_zeroDivision(expression,expected) : 
-    result = calculate_expression(expression)
+def test_zeroDivision(opA,oper,opB,expected) : 
+    result = calculate_expression(opA,oper,opB)
     assert result == expected
