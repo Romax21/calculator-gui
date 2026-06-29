2b4225195a6851c83455d55591ecf91e40888cbc
Ronaldo <rykendocool21@gmail.com>
2026-06-29 21:50:21 +0530

    Controller included in exception handling

--- main/controller/calculator_controller.py
+++ main/controller/calculator_controller.py
@@ -1,3 +1,5 @@
+from main.utils import constants
+
 class CalculatorController : 
     def __init__(self,engine):
         self.expression = ""
@@ -7,21 +9,28 @@ class CalculatorController :
     def buttonPressed(self,text) : 
         self.expression = ""
         self.result = ""
-        if text in '1234567890': 
-            self.expression = self.engine.addNumber(text)
-        elif text == '.' : 
-            self.expression = self.engine.addDecimal()
-        elif text in '+-*/%' : 
-            self.expression = self.engine.addOperator(text)
-        elif text == '1/x' : 
-            self.expression,self.result = self.engine.reciprocal()
-        elif text == 'x^2' : 
-            self.expression,self.result = self.engine.square()
-        elif text == '<=' : 
-            self.expression = self.engine.backspace()
-        elif text == '=' : 
-            self.expression,self.result = self.engine.equalCalled()
-        elif text == 'Reset' : 
+        try : 
+            if not text : 
+                raise ValueError(constants.INPUT_EMPTY_ERROR)
+            elif text.isdigit() : 
+                self.expression = self.engine.addNumber(text)
+            elif text == '.' : 
+                self.expression = self.engine.addDecimal()
+            elif text in '+-*/%' : 
+                self.expression = self.engine.addOperator(text)
+            elif text == '1/x' : 
+                self.expression,self.result = self.engine.reciprocal()
+            elif text == 'x^2' : 
+                self.expression,self.result = self.engine.square()
+            elif text == '<=' : 
+                self.expression = self.engine.backspace()
+            elif text == '=' : 
+                self.expression,self.result = self.engine.equalCalled()
+            elif text == 'Reset' : 
+                self.engine.resetScreen()
+            else : 
+                raise ValueError(constants.INVALID_INPUT_ERROR)
+        except (ValueError,ZeroDivisionError) as e : 
             self.engine.resetScreen()
-        
+            self.result = str(e)
         return (self.expression,self.result)
